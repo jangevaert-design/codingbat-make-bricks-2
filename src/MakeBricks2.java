5044673de9347efd65e276ffa9304019d96d5b3a
Jan Gevaert <jan.gevaert@gmail.com>
2020-09-11 10:42:13 -0600

    Initial commit

---
+++ src/MakeBricks2.java
@@ -0,0 +1,11 @@
+public class MakeBricks2 {
+  public boolean makeBricks(int small, int big, int goal) {
+    if (goal > big * 5 + small)
+      return false;
+
+    if ((goal % 5) > small)
+      return false;
+
+    return true;
+  }
+}
